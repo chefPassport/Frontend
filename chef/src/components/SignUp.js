3c96100777e12f77c1750523ad2c00faa1d49bc6
Chris Engel <Chris.Engel@yahoo.com>
2020-02-03 17:21:28 -0500

    styling

--- chef/src/components/SignUp.js
+++ chef/src/components/SignUp.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 // dependancy imports
 
 // material UI imports
