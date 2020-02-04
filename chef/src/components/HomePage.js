9f26108938ee05093ccdeb63597f079e43f6f0cc
Mcguffinn <deronviledwin@gmail.com>
2020-02-03 19:53:17 -0500

    init commit

---
+++ chef/src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function HomePage(){
+    return(
+    <section className="Navbar">        <h1>Simmr</h1>
+        <nav>
+            <a>Home</a>
+            <a>Profile</a>
+            <a>My Recipes</a>
+            <a>Settings</a>
+        </nav>
+    </section>
+    );
+}
