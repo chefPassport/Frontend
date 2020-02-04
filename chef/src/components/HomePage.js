ff630fa8fa03aa906d97c62cad0b20611da8a948
Mcguffinn <deronviledwin@gmail.com>
2020-02-04 01:32:57 -0500

    a good stopping point for the night (morning), added some dependencies that might not be needed we can discuss in the morning

--- chef/src/components/HomePage.js
+++ chef/src/components/HomePage.js
@@ -1,14 +1,64 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import styled ,{createGlobalStyle} from 'styled-components/macro';
 
-export default function HomePage(){
-    return(
-    <section className="Navbar">        <h1>Simmr</h1>
-        <nav>
-            <a>Home</a>
-            <a>Profile</a>
-            <a>My Recipes</a>
-            <a>Settings</a>
-        </nav>
-    </section>
-    );
+const DesktopNav = styled.nav`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-evenly;
+    align-content: center;
+
+    background: #373534;
+    color: black;
+
+    hight: 15vh;
+
+    .nav-links{
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-evenly;
+        align-content: center;
+        width: 35vw;
+        list-style: none;
+    }
+
+    .link{
+        color:#ffffff;
+        text-decoration: none;
+    }
+
+    .link:hover{
+        text-decoration: underline;
+        color: #aaf;
+        text-shadow: none;
+        -webkit-transition: 200ms linear 0s;
+        -moz-transition: 200ms linear 0s;
+        -o-transition: 200ms linear 0s;
+        transition: 200ms linear 0s;
+        outline: 0 none;
+    }
+`
+
+const HomePage =() =>{
+    return( 
+        <DesktopNav>  
+            <div className='logo'><h1>Simmr</h1></div>
+            <ul className='nav-links'>
+                <li>
+                    <Link to='/' className='link'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/' className='link'>Profile</Link>
+                </li>
+                <li>
+                    <Link to='/' className='link'>My Recipes</Link>
+                </li>
+                <li>
+                    <Link to='/' className='link'>Settings</Link>
+                </li>
+            </ul>
+        </DesktopNav> 
+    )
 }
+
+export default HomePage;
